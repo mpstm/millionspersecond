a00e63e000377db92d427fec47eab133ab2904bb
Kumar Sundaram <sundaramkumar@Kumar-Sundarams-iMac.local>
2018-06-20 23:46:55 -0400

    new changes

--- src/hello.go
+++ src/hello.go
@@ -1,5 +1,5 @@
 package main
 import "fmt"
 func main() {
-fmt.Println("Hello, BF")
+	fmt.Println("Hello, BF")
 }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-main()
+#include <stdlib.h>
+#include <string.h>
+ main(int argc, char *argv[])
 {
- printf("hello world\n");
+ // starttime
+// call strcat () 1000 times 
+// takeendtime
+// endtime = time()
+//  printf("hello world\n");
+// nameofcall, #numoftimes, timetaken
+  int count = atoi(argv[1]);
+  printf("\n count %d ", count);
+  for(int i=0;i < count;i++) {
+	int d = atoi("123");
+   }
 }
 
